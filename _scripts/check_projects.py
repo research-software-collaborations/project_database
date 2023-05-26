63924d1172f00099bde6a37dc49cd688dae584cf
David Lange <david.lange@cern.ch>
2023-05-26 16:01:18 +0200

    improve project checking

--- _scripts/check_projects.py
+++ _scripts/check_projects.py
@@ -7,7 +7,9 @@ def get_yaml(f):
         try:
             data = yaml.safe_load(stream)
         except yaml.YAMLError as exc:
+            print("Error reading yaml file",f)
             print(exc)
+            return None
     return data
 
 print("Hi there. Checking your projects")
@@ -25,16 +27,25 @@ for root, dirs, files in os.walk("./projects"):
         #append the file name to the list
         if file.endswith('yml'):
             filelist.append(os.path.join(root,file))
+n_errors=0
 
 for f in filelist:
     data = get_yaml(f)
+    if data is None:
+        n_errors+=1
+        print("Error reading yaml input file -- invalid yaml syntax?",f)
+        continue
+    if isinstance(data,str):
+        print("Error reading yaml input file -- invalid yaml syntax?",f)
+        n_errors+=1
+        continue
     found_attributes=data.keys()
     for att in found_attributes:
         if att not in all_attributes and att not in extra_attributes:
             print("Found unexpected attribute")
             print(f)
             print(att)
-            sys.exit(1)
+            n_errors+=1
         if att not in extra_attributes:
             vals = data[att]
             for v in vals:
@@ -43,19 +54,22 @@ for f in filelist:
                     print(f)
                     print(att)
                     print(v)
-                    sys.exit(1)
+                    n_errors+=1
     for att in all_attributes:
         if att not in found_attributes:
             print("Missing expected attribute")
             print(f)
             print(att)
-            sys.exit(1)
+            n_errors+=1
     for att in extra_attributes:
         if att not in found_attributes:
             print("Missing expected attribute")
             print(f)
             print(att)
-            sys.exit(1)
+            n_errors+=1
 
+if n_errors>0:
+    print("Found",n_errors,"errors")
+    sys.exit(1)
 print("Success")
 print("Checked ",str(len(filelist)),"projects")
